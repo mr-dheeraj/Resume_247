a561bd9846ddbb0e8affd9dd5c6d9ba9295932fd
DHEERAJ PRATAP SINGH <72618058+mr-dheeraj@users.noreply.github.com>
2022-06-27 22:11:42 +0530

    Update Context.js

--- src/Context.js
+++ src/Context.js
@@ -1,5 +1,5 @@
-import { createContext, useContext, useEffect, useRef, useState } from "react";
-import { useToast } from '@chakra-ui/react';
+import { createContext, useContext,  useRef, useState } from "react";
+//import { useToast } from '@chakra-ui/react';
 
 const ResumeContext = createContext();
 
